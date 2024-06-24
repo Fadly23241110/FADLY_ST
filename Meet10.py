f1b7192c6c92d0a5ff3133334eccaf48060d2969
Fadly23241110 <165535864+Fadly23241110@users.noreply.github.com>
2024-06-24 09:06:28 +0800

    Meet10.py

---
+++ Meet10.py
@@ -0,0 +1,42 @@
+# latihan perulangan
+
+#bil = 1
+
+#bil = 1 
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+
+#if kondisi
+#     aksi
+
+bil = [0,1,2,3,4] # ini menggunakan list
+print(bil)
+
+for i in bil : 
+     print (f"nilai i sekarang ->{i}")
+    
+print ("selesai")  
+
+bil2 = range(5) # ini menggunakan range 
+for i in bil2 : 
+    print (f"nilai i sekarang: {i}")
+print("selesai 2")
+
+bil3 = range(1,5) # ini menggunakan range 
+for i in bil3 : 
+    print (f"nilai i sekarang: {i}")
+print("selesai 3")
+
+# ini adalah bahasa 
+#for (int i; i <=5; i++) {
+#    aksi
+#}
