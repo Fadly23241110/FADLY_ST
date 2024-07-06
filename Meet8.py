5d9e2ace2679b3233893b98ae421c2da2524e72c
Fadly23241110 <165535864+Fadly23241110@users.noreply.github.com>
2024-07-06 17:39:13 +0800

    Meet8.py

--- Meet8.py
+++ Meet8.py
@@ -1,18 +1,15 @@
 # percabangan
 
-
 nama = input("silahkan masukkan nama : ")
 # IF kondisi : aksi
 # if nama == "edy" : print ("Dosen ganteng")
 # print("nama yang dimasukkan tidak ganteng")
-
 
 #if nama == "edy" :
  #   print("Dosen ganteng")
   #  print("Ganteng Banget")
    # print(f"Ganteng Banget {nama}")
 #print("nama yang dimasukkan tidak ganteng")
-
 
 if nama == "edy" :
     print("Dosen ganteng")
